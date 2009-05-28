51ba1f78a6ed28db91e3f04b2e55e2c9c96ae01b
Jan Zimmek <jan.zimmek@web.de>
2009-05-28 17:34:58 +0200

    create a handy view combining search, list and entries

--- templates/migrations/create.rb
+++ templates/migrations/create.rb
@@ -21,10 +21,31 @@ class Create<%= target_module %> < ActiveRecord::Migration
       t.integer :<%= target_model.underscore %>_id
       t.timestamps
     end
+    
+    
+    sql = <<-SQL
+      create view <%= entry_table %>_view as select
+        s.id as search_id,
+        e.*
+      from
+        <%= entry_table %> e
+      left join
+        <%= list_table %> l
+      on
+        l.id = e.list_id
+      left join
+        <%= search_table %> s
+      on
+        s.id = l.search_id
+    SQL
+    
+    ActiveRecord::Base.connection.execute(sql)
  
   end
  
   def self.down
+    ActiveRecord::Base.connection.execute("drop view <%= entry_table %>_view")
+    
     drop_table :<%= entry_table %>
     drop_table :<%= list_table %>
     drop_table :<%= search_table %>
